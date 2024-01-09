0cec2b31aa648fcc35a74fa20201dfcb41d87001
최우혁 <19192r8@naver.com>
2024-01-09 14:14:57 +0900

    first commit

--- android/app/src/main/java/com/korensEm/MainActivity.java
+++ android/app/src/main/java/com/korensEm/MainActivity.java
@@ -1,4 +1,4 @@
-package com.notifee;
+package com.korensEm;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/korensEm/MainApplication.java
+++ android/app/src/main/java/com/korensEm/MainApplication.java
@@ -1,4 +1,4 @@
-package com.notifee;
+package com.korensEm;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
